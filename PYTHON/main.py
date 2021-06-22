ff918f3184b8667ce2d21963ec7544561e4ea538
Héctor Tejeda <hector.tejedaihl@cencosud.cl>
2021-06-22 14:02:22 -0400

    Se deja el proceso en produccion

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,28 +1,32 @@
+from time import sleep
 from apis import (apiTipificador, apiBackOffice)
 apiConsultarCasosTipificador    = apiTipificador.apiConsultarCasosTipificador
 apiAgregarCentroCaso            = apiTipificador.apiAgregarCentroCaso
 apiBackofficeFilter             = apiBackOffice.apiBackofficeFilter
 
-data = apiConsultarCasosTipificador()
-print(len(data))
-data = data[0:300]
+try:
+    data = apiConsultarCasosTipificador()
+    print(len(data))
+    pedidos = []
+    for caso in data:
+        if caso['order_vtex'] != None:
+            pedidos.append(caso['order_vtex'])
+        elif 'pedido' in caso['pedido']:
+            pedidos.append(caso['pedido']['pedido'])
+    pedidos = apiBackofficeFilter(pedidos)
 
-pedidos = []
-for caso in data:
-    if caso['order_vtex'] != None:
-        pedidos.append(caso['order_vtex'])
-    elif 'pedido' in caso['pedido']:
-        pedidos.append(caso['pedido']['pedido'])
-pedidos = apiBackofficeFilter(pedidos)
-
-contador = 0
-for caso in data:
-    print(f"{contador}/{len(data)}")
-    if caso['order_vtex'] in pedidos:
-        centro = pedidos[caso['order_vtex']]
-        apiAgregarCentroCaso(caso['id'],centro)
-    elif 'pedido' in caso['pedido']:
-        if caso['pedido']['pedido'] in pedidos:
-            centro = pedidos[caso['pedido']['pedido']]
+    contador = 0
+    for caso in data:
+        print(f"{contador}/{len(data)}")
+        if caso['order_vtex'] in pedidos:
+            centro = pedidos[caso['order_vtex']]
             apiAgregarCentroCaso(caso['id'],centro)
-    contador += 1
+        elif 'pedido' in caso['pedido']:
+            if caso['pedido']['pedido'] in pedidos:
+                centro = pedidos[caso['pedido']['pedido']]
+                apiAgregarCentroCaso(caso['id'],centro)
+        contador += 1
+except Exception as e:
+    print(e)
+    print('No hay casos')
+    sleep(10)
